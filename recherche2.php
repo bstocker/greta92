e013e632e9538a62f7d565f2430b621a953ea9a5
bstocker <boris.stocker@gmail.com>
2023-03-01 12:07:59 +0100

    aaa

---
+++ recherche2.php
@@ -0,0 +1,30 @@
+<html>
+<body>
+<h1>Formulaire de recherche</h1>
+<form action="#" method="GET">
+
+<p>ANNEE ? <SELECT name="annee">
+
+<?php
+//Lister le contenu de la table movies
+
+//1° - Connexion à la BDD
+$base = new PDO('mysql:host=localhost; dbname=id20215400_cnamgreta', 'id20215400_boris', '=nq^^=vKz?FRiz7-');
+$base->exec("SET CHARACTER SET utf8");
+
+//2° - Préparation de requette et execution
+$retour = $base->query('SELECT DISTINCT annee FROM movies;');
+
+//3° - Lecture du resultat de la requette
+while ($data = $retour->fetch()){
+echo "<option>".$data['annee']."</option>";
+}
+
+?>
+</SELECT>
+</p>
+
+<p><input type="submit"/></p>
+</form>
+</body>
+</html>
