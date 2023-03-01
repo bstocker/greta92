a57414958e747064a40714f5e66c41ae08825fb1
bstocker <boris.stocker@gmail.com>
2023-03-01 11:39:18 +0100

    lll

--- insert.php
+++ insert.php
@@ -7,12 +7,12 @@ $TITRE= $_GET['titre'];
 $GENRE = $_GET['genre'];
 $ANNEE = $_GET['annee'];
 
-echo $ID ." ".$TITRE." ".$GENRE." ".$ANNEE; 
+echo $TITRE." ".$GENRE." ".$ANNEE; 
 
 //1° - Connexion à la BDD
 $base = new PDO('mysql:host=localhost; dbname=id20215400_cnamgreta', 'id20215400_boris', '=nq^^=vKz?FRiz7-');
 $base->exec("SET CHARACTER SET utf8");
-$sql = 'INSERT INTO films VALUES(NULL,"'.$TITRE.'","'.$GENRE.'",'.$ANNEE.')';
+$sql = 'INSERT INTO movies VALUES(NULL,"'.$TITRE.'","'.$GENRE.'",'.$ANNEE.')';
 //2° - Préparation de requette et execution
 $retour = $base->query($sql);
 
